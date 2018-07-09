2e735d9abed157fa4347d561a66b5f4a64ff8da3
Jerry Tan <jerrytansk@gmail.com>
2018-07-10 02:51:14 +0800

    newline at end of config

--- autovpn.go
+++ autovpn.go
@@ -54,9 +54,9 @@ func main() {
 		}
 
 		// Add in DNS resolv.conf scripts for linux servers
-		conf2 := `script-security 2
-up /etc/openvpn/update-resolv-conf
-down /etc/openvpn/update-resolv-conf`
+		conf2 := "script-security 2\n" +
+				 "up /etc/openvpn/update-resolv-conf\n" +
+				 "down /etc/openvpn/update-resolv-conf\n"
 		conf = append(conf, conf2...)
 		// fmt.Printf("%q\n", conf)
 
